e3a3ddefa5813e54d196d4681d6e2a6448ec184d
eehd80 <eehd80yul@gmail.com>
2021-02-04 13:52:40 +0900

    setting gnb

--- !setting/dist/js/comm.js
+++ !setting/dist/js/comm.js
@@ -1,36 +1,46 @@
 $(function(){
+  // header sticky
+  $(window).scroll(function(){
+    if($(this).scrollTop() > 50){
+      $('header').addClass('bg-light navbar-light').removeClass('navbar-dark')
+    }else{
+      $('header').removeClass('bg-light navbar-light').addClass('navbar-dark')
+    }
+  })
+  $(window).trigger('scroll')
 
-    // slide-top
-    var swiper = new Swiper('.slide-top', {
-      spaceBetween: 30,
-      centeredSlides: true,
-      loop: true,
-      effect: 'fade',
-      // autoplay: {
-      //   delay: 2500,
-      //   disableOnInteraction: false,
-      // },
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-      },
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
+
+  // slide-top
+  var swiper = new Swiper('.slide-top', {
+    spaceBetween: 30,
+    centeredSlides: true,
+    loop: true,
+    effect: 'fade',
+    // autoplay: {
+    //   delay: 2500,
+    //   disableOnInteraction: false,
+    // },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
+  // scrollUp
+  $.scrollUp({
+      scrollName: 'scrollUp', // Element ID
+      topDistance: '300', // Distance from top before showing element (px)
+      topSpeed: 300, // Speed back to top (ms)
+      animation: 'fade', // Fade, slide, none
+      animationInSpeed: 200, // Animation in speed (ms)
+      animationOutSpeed: 200, // Animation out speed (ms)
+      scrollText: '페이지 위로 이동', // Text for element
+      activeOverlay: false, // Set CSS color to display scrollUp active point, e.g '#00FFFF'
     });
-
-    // scrollUp
-    $.scrollUp({
-        scrollName: 'scrollUp', // Element ID
-        topDistance: '300', // Distance from top before showing element (px)
-        topSpeed: 300, // Speed back to top (ms)
-        animation: 'fade', // Fade, slide, none
-        animationInSpeed: 200, // Animation in speed (ms)
-        animationOutSpeed: 200, // Animation out speed (ms)
-        scrollText: '페이지 위로 이동', // Text for element
-        activeOverlay: false, // Set CSS color to display scrollUp active point, e.g '#00FFFF'
-      });
 
 
 
